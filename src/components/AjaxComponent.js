314b7d9daa4d20650c254c58e7749b78675097a8
Andrés Felipe <camachorodriguezandresfelipe@gmail.com>
2024-04-07 00:35:16 -0500

    modificaciones

--- src/components/AjaxComponent.js
+++ src/components/AjaxComponent.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, { useEffect, useState } from 'react'
 
 
@@ -5,6 +6,8 @@ import React, { useEffect, useState } from 'react'
 export const AjaxComponent = () => {
 
     const [ usuarios, setUsuarios ] = useState([]);
+
+    const [ cargando, setCargando ] = useState(true);
 
     // Genirico / basico:
     // const getUsuariosEstaticos = () => {
@@ -68,10 +71,17 @@ export const AjaxComponent = () => {
     // }
 
     const getUsuariosAjasAW = async () => {
-        const peticion = await fetch("https://reqres.in/api/users?page=1");
-        const { data } = await peticion.json();
+        setTimeout(async () => {
+            // alert("Hola")
+            // ejecutar el codigo en ves de mostrar el alert:
+            const peticion = await fetch("https://reqres.in/api/users?page=1");
+            const { data } = await peticion.json();
 
-        setUsuarios(data);
+            setUsuarios(data);
+            setCargando(false);
+        }, 5000);
+
+
     }
 
     useEffect(() => {
@@ -80,18 +90,34 @@ export const AjaxComponent = () => {
         getUsuariosAjasAW();
     }, [])
 
-    return (
-        <div>
-            <h2> Listado de usuarios via Ajax</h2>
-            <ol className='usuarios'>
-                {
-                    usuarios.map(usuario => {
-                        console.log(usuario);
-                        return <li key={usuario.id}>{usuario.first_name} {usuario.last_name}</li>;
-                    })
-                }
+    // condicion:
+    if (cargando === true) {
+        // Cuando todo esta cargando:
+        return (
+            <div className='cargando'>
+                cargando datos...
+            </div>
+        );
 
-            </ol>
-        </div>
-    )
+    } else {
+        // Cuando todo a ido bien.
+        return (
+            <div>
+                <h2> Listado de usuarios via Ajax</h2>
+                <ol className='usuarios'>
+                    {
+                        usuarios.map(usuario => {
+                            console.log(usuario);
+                            return (<li key={usuario.id}>
+                                <img src={usuario.avatar} width="20" />
+                                &nbsp;
+                                {usuario.first_name} {usuario.last_name}
+                            </li>);
+                        })
+                    }
+
+                </ol>
+            </div>
+        )
+    }
 }
